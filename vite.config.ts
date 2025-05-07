0bbbf8b7208aa48116f83744a0a7636c3a8e90f7
Orrie-Dan <d.nkusi@alustudent.com>
2025-05-07 10:07:03 +0200

    removed some bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -21,6 +22,7 @@ export default defineConfig({
       }
     }
   },
+
   optimizeDeps: {
     include: [
       'react', 
@@ -37,14 +39,15 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          mui: ['@mui/material', '@emotion/react', '@emotion/styled']
+          mui: ['@mui/material','@emotion/react', '@emotion/styled']
         }
       }
     }
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': '/src',
+      '@mui/styled-engine': '@mui/styled-engine-sc'
     }
   },
   base: '/',
